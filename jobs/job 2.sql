5c017c3a36ed9954ac2c772307e885a295d75d9a
ali-bousrira <146709104+ali-bousrira@users.noreply.github.com>
2024-12-19 14:57:24 +0100

    Update job 2.sql

--- jobs/job 2.sql
+++ jobs/job 2.sql
@@ -1,9 +1,9 @@
 Job 2 : 
-1. SELECT Country FROM world WHERE Country like "B%";
-2. SELECT Country FROM world WHERE Country like "Al%";
-3. SELECT Country FROM world WHERE Country like "%y ";
-4. SELECT Country FROM world WHERE Country like "%land ";
-5. SELECT Country FROM world WHERE Country LIKE "%w%";
-6. SELECT Country FROM world WHERE Country LIKE "%oo%" or name LIKE "%ee%";
+1. SELECT Country FROM world WHERE Country like 'B%';
+2. SELECT Country FROM world WHERE Country like 'Al%';
+3. SELECT Country FROM world WHERE Country like '%y ';
+4. SELECT Country FROM world WHERE Country like '%land ';
+5. SELECT Country FROM world WHERE Country LIKE '%w%';
+6. SELECT Country FROM world WHERE Country LIKE '%oo%' or name LIKE '%ee%';
 7. SELECT Country FROM world WHERE LENGTH(Country) - LENGTH(REPLACE(Country, 'a', '')) >= 3;
-8. SELECT Country FROM world WHERE Country LIKE "_r%";
+8. SELECT Country FROM world WHERE Country LIKE '_r%';
