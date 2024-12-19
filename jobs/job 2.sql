2f1a951b2beed2938cc20fe5c144177293f49470
ali-bousrira <146709104+ali-bousrira@users.noreply.github.com>
2024-12-19 14:48:12 +0100

    Update job 2.sql

--- jobs/job 2.sql
+++ jobs/job 2.sql
@@ -1,8 +1,8 @@
 Job 2 : 
-1. SELECT name FROM world WHERE name = "B%";
-2. SELECT name FROM world WHERE name = "Al%";
-3. SELECT name FROM world WHERE name = "%y";
-4. SELECT name FROM world WHERE name = "%land";
+1. SELECT name FROM world WHERE name like "B%";
+2. SELECT name FROM world WHERE name like "Al%";
+3. SELECT name FROM world WHERE name like "%y";
+4. SELECT name FROM world WHERE name like "%land";
 5. SELECT name FROM world WHERE name LIKE "%w%";
 6. SELECT name FROM world WHERE name LIKE "%oo%" or name LIKE "%ee%";
 7. SELECT name FROM world WHERE LENGTH(name) - LENGTH(REPLACE(name, 'a', '')) >= 3;
